b4bef5fc35e523def9716821ee5d7d93eebbede0
JDRL555 <joshuleal3@gmail.com>
2023-05-26 11:21:33 -0400

    upgraded the first database structure

--- src/database/database1.sql
+++ src/database/database1.sql
@@ -3,91 +3,76 @@ CREATE DATABASE IF NOT EXISTS mavet_db;
 
 USE mavet_db;
 
-DROP TABLE IF EXISTS registro;
-CREATE TABLE IF NOT EXISTS registro( 
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users( 
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name_user TEXT NOT NULL,
+  last_name_user TEXT NOT NULL,
+  email_user VARCHAR(100) NOT NULL,
+  phone_user TINYINT(12) NOT NULL,
+  specialty_user SET("Estudiante", "Profesor") NOT NULL,
+  type_user SET("Estandar", "Administrador") NOT NULL,
+  avatar_user VARCHAR(100) DEFAULT '',
+  username_user VARCHAR(50) NOT NULL,
+  password_user VARCHAR(255) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_DATE,
 
-    -- Columnas
-    id_usuario INT UNSIGNED AUTO_INCREMENT,
-    nombres_usuario TEXT NOT NULL,
-    apellidos_usuario TEXT NOT NULL,
-    correo_usuario VARCHAR(80) NOT NULL,
-    pais_usuario TEXT NOT NULL,
-    especialidad_usuario TEXT NOT NULL,
-    nombre_de_usuario VARCHAR(30) NOT NULL,
-    contraseña_usuario VARCHAR(100) NOT NULL,
-    confirmar_clave VARCHAR(100) NOT NULL,
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-
-    -- Llaves
-    PRIMARY KEY(id_usuario),
-    CONSTRAINT UNIQUE(correo_usuario)
-
+  PRIMARY KEY(id),
+  CONSTRAINT UNIQUE(email_user),
+  CONSTRAINT UNIQUE(username_user)
 );
 
+DROP TABLE IF EXISTS works_art;
+CREATE TABLE IF NOT EXISTS works_art(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_user INT UNSIGNED NOT NULL,
+  title_work TEXT NOT NULL,
+  img_work VARCHAR(100) NOT NULL,
+  description_work TEXT NOT NULL,
+  type_work SET("Pintura", "Dibujo", "Fotografia") NOT NULL,
+  likes_work INT DEFAULT 0,
+  created_at DATETIME DEFAULT CURRENT_DATE,
 
-DROP TABLE IF EXISTS inicio_sesion;
-CREATE TABLE IF NOT EXISTS inicio_sesion(
-	
-    -- Columnas
-    id_sesion INT UNSIGNED AUTO_INCREMENT,
-	correo_id VARCHAR(80) NOT NULL,
-    contraseña_id VARCHAR(100) NOT NULL,
-	fecha_ingreso TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-	
-    -- Llaves
-    PRIMARY KEY(id_sesion),
-    FOREIGN KEY(correo_id) REFERENCES registro(id_usuario),
-    FOREIGN KEY(contraseña_id) REFERENCES registro(id_usuario)
-
+  PRIMARY KEY(id),
+  FOREIGN KEY(id_user) REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS comments;
+CREATE TABLE IF NOT EXISTS comments(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_user INT UNSIGNED NOT NULL,
+  id_work INT UNSIGNED NOT NULL,
+  content_comment TEXT NOT NULL,
+  likes_comment INT DEFAULT 0,
+  created_at DATETIME DEFAULT CURRENT_DATE,
 
-DROP TABLE IF EXISTS publicaciones;
-CREATE TABLE IF NOT EXISTS publicaciones(
-
-    -- Columnas
-    id_publicacion INT UNSIGNED AUTO_INCREMENT,
-    -- tipo_de_publicacion TEXT NOT NULL,
-    titulo_publicacion TEXT NOT NULL,
-    -- imagen_publicacion
-    descripcion_publicacion TEXT NOT NULL,
-    reacciones_publicacion INT UNSIGNED AUTO_INCREMENT,
-    comentarios_publicacion TEXT NOT NULL,
-    fecha_publicacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-
-    -- Llaves
-    PRIMARY KEY(id_publicacion)
-
+  PRIMARY KEY(id),
+  FOREIGN KEY(id_user) REFERENCES users(id),
+  FOREIGN KEY(id_work) REFERENCES works_art(id)
 );
 
+DROP TABLE IF EXISTS events;
+CREATE TABLE IF NOT EXISTS events(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  img_event TEXT NOT NULL,
+  title_event TEXT NOT NULL,
+  description_event TEXT NOT NULL,
+  likes_event INT DEFAULT 0,
+  date_event DATETIME DEFAULT CURRENT_DATE,
 
-DROP TABLE IF EXISTS eventos;
-CREATE TABLE IF NOT EXISTS eventos(
-
-    -- Columnas
-    id_evento INT UNSIGNED AUTO_INCREMENT,
-    titulo_evento TEXT NOT NULL,
-    -- imagen_evento
-    descripcion_eventos TEXT NOT NULL,
-    fecha_eventos TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-
-    -- Llaves
-    PRIMARY KEY(id_evento)
-
+  PRIMARY KEY(id)
 );
 
+DROP TABLE IF EXISTS courses;
+CREATE TABLE IF NOT EXISTS courses(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_teacher INT UNSIGNED NOT NULL,
+  title_course TEXT NOT NULL,
+  img_course TEXT NOT NULL,
+  description_course TEXT NOT NULL,
+  price_course FLOAT DEFAULT 0,
+  date_course DATETIME DEFAULT CURRENT_DATE,
 
-DROP TABLE IF EXISTS cursos;
-CREATE TABLE IF NOT EXISTS cursos(
-
-    -- Columnas
-    id_cursos INT UNSIGNED AUTO_INCREMENT,
-    titulo_cursos TEXT NOT NULL,
-    -- imagen_cursos
-    descripcion_cursos TEXT NOT NULL,
-    fecha_cursos TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-
-    -- Llaves
-    PRIMARY KEY(id_cursos)
-
+  PRIMARY KEY(id),
+  FOREIGN KEY(id_teacher) REFERENCES users(id)
 );
