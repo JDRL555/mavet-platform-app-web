50451ef5f1ca38ea8976926fa876808f53d2ab73
carlosweb07 <carlosmirabalv@gmail.com>
2023-06-07 23:28:35 -0400

    Created the controller to upload posts (first version)

--- src/routes/posts_routes.py
+++ src/routes/posts_routes.py
@@ -1,7 +1,8 @@
-from flask            import Blueprint, render_template, redirect
-from flask_login      import current_user
-from models.Category  import Category
-from utils.db         import db
+from flask               import Blueprint, render_template, redirect, request
+from flask_login         import current_user
+from models.Category     import Category
+from models.mavet_models import Works_art
+from utils.db            import db
 
 posts_router = Blueprint("posts", __name__)
 
@@ -51,5 +52,35 @@ def renderPosts():
       }
     }
     return render_template("posts.html", data=data)
+  except TypeError:
+    return redirect("/signin")
+
+@posts_router.route("/new/post", methods=['POST'])
+def createPost():
+  try:
+    result = Category.getAll(db=db)
+    data = {
+      "inputs": [
+        {"name": "title", "type": "text", "label": "Titulo de la obra"},
+        {"name": "description", "type": "text", "label": "Descripción de la obra"},
+        {"name": "category", "options": result, "label": "Categoría de la obra"},
+        {"name": "img", "type": "file", "label": "Adjunta la imagen de la obra"},
+      ]
+    }
+
+    post_info = {
+      "title": request.form['title'],
+      "description": request.form['description'],
+      "category": request.form['category'],
+      "img": request.form['img']
+    }
+
+    new_post = Works_art(post_info['title'], post_info['description'], post_info['category'], post_info['img'])
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect("/posts")
+
   except TypeError:
     return redirect("/signin")
