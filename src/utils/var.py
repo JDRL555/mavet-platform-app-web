0a319d7aaade2372470d1507ea1c2ae0eb803b9d
JDRL555 <joshuleal3@gmail.com>
2023-06-22 12:43:03 -0400

    changed the API to cloudinary

--- src/utils/var.py
+++ src/utils/var.py
@@ -3,11 +3,7 @@ from os import getenv
 
 load_dotenv()
 
-#imgbb API
-API_KEY       = getenv("API_KEY") 
-API_URL       = getenv("API_URL")
-
-#cloudinary
+#cloudinary API
 CLOUD_NAME    = getenv("CLOUD_NAME")
 API_KEY_C     = getenv("API_KEY_C")
 API_SECRET    = getenv("API_SECRET")
