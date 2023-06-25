182ecff9c2f2e217ea133cbd26d63f775eefde78
JDRL555 <joshuleal3@gmail.com>
2023-06-24 22:48:43 -0400

    removed the match statement

--- src/routes/admin_routes.py
+++ src/routes/admin_routes.py
@@ -23,97 +23,96 @@ def admin():
 def modal():
   req   = request.get_json()
   modal = get_template_attribute("macros/modal.html", "modal")
-  match req["from"]:
-    case "Usuarios":
-      if req["option"] == "Listar":  
-        users = User.getAll(db=db)
-        if not users: 
-          data["modal"]["Usuarios"]["Listar"]["data"] = []
-        else:  
-          columns_users   = data["modal"]["Usuarios"]["Listar"]["filters"]
-          registers_users = [dict(register = list(register.values())) for register in users]
-          
-          users_data = {
-            "cols": columns_users,
-            "rows": registers_users
-          }
-          
-          data["modal"]["Usuarios"]["Listar"]["data"] = users_data
-  
-        modal = modal(data["modal"]["Usuarios"]["Listar"])
-      if req["option"] == "Crear":      
+  if req["form"] == "Usuarios":
+    if req["option"] == "Listar":  
+      users = User.getAll(db=db)
+      if not users: 
+        data["modal"]["Usuarios"]["Listar"]["data"] = []
+      else:  
+        columns_users   = data["modal"]["Usuarios"]["Listar"]["filters"]
+        registers_users = [dict(register = list(register.values())) for register in users]
         
-        types       = Type_user.getAll(db=db)
-        specialties = Specialty_user.getAll(db=db)
-        data["modal"]["Usuarios"]["Crear"]["inputs"][7]["options"] = specialties
-        data["modal"]["Usuarios"]["Crear"]["inputs"][8]["options"] = types
+        users_data = {
+          "cols": columns_users,
+          "rows": registers_users
+        }
+        
+        data["modal"]["Usuarios"]["Listar"]["data"] = users_data
 
-        modal = modal(data["modal"]["Usuarios"]["Crear"])
-      if req["option"] == "Actualizar": modal = modal(data["modal"]["Usuarios"]["Actualizar"])
-      if req["option"] == "Eliminar":   modal = modal(data["modal"]["Usuarios"]["Eliminar"])
+      modal = modal(data["modal"]["Usuarios"]["Listar"])
+    if req["option"] == "Crear":      
       
-    case "Eventos":
-      if req["option"] == "Listar":     
-        events = Event.getAll(db=db)
-        if not events: 
-          data["modal"]["Eventos"]["Listar"]["data"] = []
-        else:  
-          columns_events   = data["modal"]["Eventos"]["Listar"]["filters"]
-          registers_events = [dict(register = list(register.values())) for register in events]
-          
-          events_data = {
-            "cols": columns_events,
-            "rows": registers_events
-          }
-          
-          data["modal"]["Eventos"]["Listar"]["data"] = events_data
-  
-        modal = modal(data["modal"]["Eventos"]["Listar"])
-      if req["option"] == "Crear":      modal = modal(data["modal"]["Eventos"]["Crear"])
-      if req["option"] == "Actualizar": modal = modal(data["modal"]["Eventos"]["Actualizar"])
-      if req["option"] == "Eliminar":   modal = modal(data["modal"]["Eventos"]["Eliminar"])
-      
-    case "Cursos":
-      if req["option"] == "Listar":     
-        courses = Course.getAll(db=db)
-        if not courses: 
-          data["modal"]["Cursos"]["Listar"]["data"] = []
-        else:
-          columns_courses   = data["modal"]["Cursos"]["Listar"]["filters"]
-          registers_courses = [dict(register = list(register.values())) for register in courses]
-          
-          courses_data = {
-            "cols": columns_courses,
-            "rows": registers_courses
-          }
-          
-          data["modal"]["Cursos"]["Listar"]["data"] = courses_data
-  
-        modal = modal(data["modal"]["Cursos"]["Listar"])
-      if req["option"] == "Crear":      modal = modal(data["modal"]["Cursos"]["Crear"])
-      if req["option"] == "Actualizar": modal = modal(data["modal"]["Cursos"]["Actualizar"])
-      if req["option"] == "Eliminar":   modal = modal(data["modal"]["Cursos"]["Eliminar"])
-      
-    case "Categorias":
-      if req["option"] == "Listar":  
-        categories = Category.getAll(db=db)
-        if not categories: 
-          data["modal"]["Categorias"]["Listar"]["data"] = []
-        else:
-          columns_categories   = data["modal"]["Categorias"]["Listar"]["filters"]
-          registers_categories = [dict(register = list(register.values())) for register in categories]
-          
-          categories_data = {
-            "cols": columns_categories,
-            "rows": registers_categories
-          }
-          
-          data["modal"]["Categorias"]["Listar"]["data"] = categories_data   
-  
-        modal = modal(data["modal"]["Categorias"]["Listar"])
-      if req["option"] == "Crear":      modal = modal(data["modal"]["Categorias"]["Crear"])
-      if req["option"] == "Actualizar": modal = modal(data["modal"]["Categorias"]["Actualizar"])
-      if req["option"] == "Eliminar":   modal = modal(data["modal"]["Categorias"]["Eliminar"])
+      types       = Type_user.getAll(db=db)
+      specialties = Specialty_user.getAll(db=db)
+      data["modal"]["Usuarios"]["Crear"]["inputs"][7]["options"] = specialties
+      data["modal"]["Usuarios"]["Crear"]["inputs"][8]["options"] = types
+
+      modal = modal(data["modal"]["Usuarios"]["Crear"])
+    if req["option"] == "Actualizar": modal = modal(data["modal"]["Usuarios"]["Actualizar"])
+    if req["option"] == "Eliminar":   modal = modal(data["modal"]["Usuarios"]["Eliminar"])
+    
+  elif req["form"] == "Eventos":
+    if req["option"] == "Listar":     
+      events = Event.getAll(db=db)
+      if not events: 
+        data["modal"]["Eventos"]["Listar"]["data"] = []
+      else:  
+        columns_events   = data["modal"]["Eventos"]["Listar"]["filters"]
+        registers_events = [dict(register = list(register.values())) for register in events]
+        
+        events_data = {
+          "cols": columns_events,
+          "rows": registers_events
+        }
+        
+        data["modal"]["Eventos"]["Listar"]["data"] = events_data
+
+      modal = modal(data["modal"]["Eventos"]["Listar"])
+    if req["option"] == "Crear":      modal = modal(data["modal"]["Eventos"]["Crear"])
+    if req["option"] == "Actualizar": modal = modal(data["modal"]["Eventos"]["Actualizar"])
+    if req["option"] == "Eliminar":   modal = modal(data["modal"]["Eventos"]["Eliminar"])
+    
+  elif req["form"] == "Cursos":
+    if req["option"] == "Listar":     
+      courses = Course.getAll(db=db)
+      if not courses: 
+        data["modal"]["Cursos"]["Listar"]["data"] = []
+      else:
+        columns_courses   = data["modal"]["Cursos"]["Listar"]["filters"]
+        registers_courses = [dict(register = list(register.values())) for register in courses]
+        
+        courses_data = {
+          "cols": columns_courses,
+          "rows": registers_courses
+        }
+        
+        data["modal"]["Cursos"]["Listar"]["data"] = courses_data
+
+      modal = modal(data["modal"]["Cursos"]["Listar"])
+    if req["option"] == "Crear":      modal = modal(data["modal"]["Cursos"]["Crear"])
+    if req["option"] == "Actualizar": modal = modal(data["modal"]["Cursos"]["Actualizar"])
+    if req["option"] == "Eliminar":   modal = modal(data["modal"]["Cursos"]["Eliminar"])
+    
+  elif req["form"] == "Categorias":
+    if req["option"] == "Listar":  
+      categories = Category.getAll(db=db)
+      if not categories: 
+        data["modal"]["Categorias"]["Listar"]["data"] = []
+      else:
+        columns_categories   = data["modal"]["Categorias"]["Listar"]["filters"]
+        registers_categories = [dict(register = list(register.values())) for register in categories]
+        
+        categories_data = {
+          "cols": columns_categories,
+          "rows": registers_categories
+        }
+        
+        data["modal"]["Categorias"]["Listar"]["data"] = categories_data   
+
+      modal = modal(data["modal"]["Categorias"]["Listar"])
+    if req["option"] == "Crear":      modal = modal(data["modal"]["Categorias"]["Crear"])
+    if req["option"] == "Actualizar": modal = modal(data["modal"]["Categorias"]["Actualizar"])
+    if req["option"] == "Eliminar":   modal = modal(data["modal"]["Categorias"]["Eliminar"])
       
   response = Response(modal, headers={'Access-Control-Allow-Origin': "*"})
   return response
